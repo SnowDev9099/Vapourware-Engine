3e7f9beeed632fdf238cf781376801c7b7f94407
SnowDev9099 <169009311+SnowDev9099@users.noreply.github.com>
2024-09-19 13:03:57 -0400

    Update and rename app.js to Game.js

---
+++ Game.js
@@ -0,0 +1,17 @@
+// game.js
+const game = new Phaser.Game('100%', '100%', Phaser.AUTO, 'gameCanvas', {
+    preload: () => {
+    
+    },
+    create: () => {
+        
+    },
+    update: (time, delta) => {
+   
+    }
+});
+
+// Add event listeners for user input
+window.addEventListener('keydown', (event) => {
+    
+});
